a77faf211c90f27b2fd6bcaf8921ad65c5a502b9
Yikai Guo <100450691@alumnos.uc3m.es>
2022-10-21 18:17:50 +0200

    blur math implementation not working...

--- aos/gaussianbluraos.cpp
+++ aos/gaussianbluraos.cpp
@@ -1,9 +1,11 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <cmath>
 #include <stdint.h>
 #include "../common/common.h"
 #include "aos.h"
+
 
 using namespace std;
 
@@ -17,7 +19,7 @@ void ImageAOS::ApplyGaussianBlur() {
         {1, 4, 7, 4, 1}
     };
 
-    uint16_t w = 273;
+    float w = 273.0;
 
     // 1. Copy the original image (bmpPixelsData) to a new image called temporalPixelsForConvolution &
     //    resize temporalPixelsForConvolution to be 4 pixels bigger in each side and fill it with 0s.
@@ -27,14 +29,23 @@ void ImageAOS::ApplyGaussianBlur() {
         for (long unsigned int j = 0; j < bmpPixelsData[0].size(); j++)
             temporalPixelsForConvolution[i + 2][j + 2] = bmpPixelsData[i][j];
 
-
     // 2. Iterate over the temporalPixelsForConvolution starting: i = 2, j = 2
     //    and ending: i = temporalPixelsForConvolution.size() - 2, j = temporalPixelsForConvolution[0].size() - 2
     for(long unsigned int i = 2; i < temporalPixelsForConvolution.size() - 2; i++){
         for(long unsigned int j = 2; j < temporalPixelsForConvolution[0].size() - 2;j++){
             // 3. For each pixel, apply the convolution operation and store the result in the original image (bmpPixelsData)
             // store it in bmpPixelsData[i-2][j-2]
-            bmpPixelsData[i-2][j-2].red = 9999;
+            int blue = 0, green = 0, red = 0;
+            for(int8_t s = -2; s < 3; s++){
+                for(int8_t t = -2; t < 3; t++){
+                    blue += temporalPixelsForConvolution[i+s][j+t].blue * kernel[s+2][t+2];
+                    green += temporalPixelsForConvolution[i+s][j+t].green * kernel[s+2][t+2];
+                    red += temporalPixelsForConvolution[i+s][j+t].red * kernel[s+2][t+2];
+                }
+            }
+            bmpPixelsData[i-2][j-2].blue = floor(blue/w + 0.5);
+            bmpPixelsData[i-2][j-2].green = floor(green/w + 0.5);
+            bmpPixelsData[i-2][j-2].red = floor(red/w + 0.5);
         }
     }
 
--- aos/imgaos.cpp
+++ aos/imgaos.cpp
@@ -8,17 +8,17 @@
 int main () {
     ImageAOS imgaos;
 
-    imgaos.ReadBitmapFile("../images/amphora.bmp");
+    imgaos.ReadBitmapFile("../images/landscape.bmp");
     imgaos.ApplyGaussianBlur();
     // imgaos.ToGrayScale();
 
-    // BmpPixels bmpPixelsData = imgaos.GetBitmapPixelsData();
+    BmpPixels bmpPixelsData = imgaos.GetBitmapPixelsData();
 
-    // for (int i = 0; i < 5; i++) {
-    //     std::cout << "Pixel " << i << ": " << std::to_string(bmpPixelsData[0][i].red) << " " << std::to_string(bmpPixelsData[0][i].green) << " " << std::to_string(bmpPixelsData[0][i].blue) << std::endl;
-    // }
+    for (int i = 0; i < 5; i++) {
+        std::cout << "Pixel " << i << ": " << std::to_string(bmpPixelsData[0][i].red) << " " << std::to_string(bmpPixelsData[0][i].green) << " " << std::to_string(bmpPixelsData[0][i].blue) << std::endl;
+    }
 
-    // imgaos.WriteBitmapFile("../images/amphora-gray.bmp");
+    imgaos.WriteBitmapFile("../images/landscape-blur.bmp");
 
     return 0;
 }

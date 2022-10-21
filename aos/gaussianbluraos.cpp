3af8edc91942bcd51db5883108f9f1c63915cba9
aaronespasa <aaespasa@gmail.com>
2022-10-21 17:48:44 +0200

    Adding padding to the temporal matrix

--- aos/gaussianbluraos.cpp
+++ aos/gaussianbluraos.cpp
@@ -7,12 +7,8 @@
 
 using namespace std;
 
-ImageAOS imgaos;
-
-
 
 void ImageAOS::ApplyGaussianBlur() {
-    
     uint8_t kernel[5][5] = {
         {1, 4, 7, 4, 1},
         {4, 16, 26, 16, 4},
@@ -23,58 +19,22 @@ void ImageAOS::ApplyGaussianBlur() {
 
     uint16_t w = 273;
 
+    // 1. Copy the original image (bmpPixelsData) to a new image called temporalPixelsForConvolution &
+    //    resize temporalPixelsForConvolution to be 4 pixels bigger in each side and fill it with 0s.
+    vector<vector<Pixel>> temporalPixelsForConvolution(bmpPixelsData.size() + 4, vector<Pixel>(bmpPixelsData[0].size() + 4));
+
+    for (long unsigned int i = 0; i < bmpPixelsData.size(); i++)
+        for (long unsigned int j = 0; j < bmpPixelsData[0].size(); j++)
+            temporalPixelsForConvolution[i + 2][j + 2] = bmpPixelsData[i][j];
 
 
-    BmpPixels createTemporalPixelsForConvolution(BmpPixels originalBmp){
-        BmpPixels temporalPixels;
-        for (int i = 0; i < originalBmp.size(); i++) {
-            vector<Pixel> row;
-            for (int j = 0; j < originalBmp[i].size(); j++) {
-                Pixel pixel;
-                pixel.blue = 0;
-                pixel.green = 0;
-                pixel.red = 0;
-                row.push_back(pixel);
-            }
-            temporalPixels.push_back(row);
-        }
-        return temporalPixels;
-    }
-
-
-
-    // 1. Copy the original image (bmpPixelsData) to a new image called temporalPixelsForConvolution
-
-    int originalHeight = bmpPixelsData.size();
-    int originalWidth = bmpPixelsData[0].size();
-    BmpPixels temporalPixelsForConvolution = bmpPixelsData;
-
-
-
-    // 2. Resize temporalPixelsForConvolution to be 4 pixels bigger in each side and fill it with 0s.
-    
-    temporalPixelsForConvolution.resize(originalHeight + 4);
-    for (int i = 0; i < originalHeight + 4; i++) {
-        temporalPixelsForConvolution[i].resize(originalWidth + 4);
-    }
-    for (int i = 0; i < originalHeight + 4; i++) {
-        for (int j = 0; j < originalWidth + 4; j++) {
-            temporalPixelsForConvolution[i][j].red = 0;
-            temporalPixelsForConvolution[i][j].green = 0;
-            temporalPixelsForConvolution[i][j].blue = 0;
-        }
-    }
-
-
-
-    // 3. Iterate over the temporalPixelsForConvolution starting: i = 2, j = 2
+    // 2. Iterate over the temporalPixelsForConvolution starting: i = 2, j = 2
     //    and ending: i = temporalPixelsForConvolution.size() - 2, j = temporalPixelsForConvolution[0].size() - 2
-
     for(long unsigned int i = 2; i < temporalPixelsForConvolution.size() - 2; i++){
         for(long unsigned int j = 2; j < temporalPixelsForConvolution[0].size() - 2;j++){
-            // 4. For each pixel, apply the convolution operation and store the result in the original image (bmpPixelsData)
+            // 3. For each pixel, apply the convolution operation and store the result in the original image (bmpPixelsData)
             // store it in bmpPixelsData[i-2][j-2]
-            bmpPixelsData[i-2][j-2].red = 
+            bmpPixelsData[i-2][j-2].red = 9999;
         }
     }
 

a55cdb3e495596e7b506b7970b88341aa2780a5b
Yikai Guo <100450691@alumnos.uc3m.es>
2022-10-21 17:28:32 +0200

    Adding GitHub Copilot resized function

--- aos/gaussianbluraos.cpp
+++ aos/gaussianbluraos.cpp
@@ -21,10 +21,51 @@ void ImageAOS::ApplyGaussianBlur() {
         {1, 4, 7, 4, 1}
     };
 
-    int w = 273;
+    uint16_t w = 273;
+
+
+
+    BmpPixels createTemporalPixelsForConvolution(BmpPixels originalBmp){
+        BmpPixels temporalPixels;
+        for (int i = 0; i < originalBmp.size(); i++) {
+            vector<Pixel> row;
+            for (int j = 0; j < originalBmp[i].size(); j++) {
+                Pixel pixel;
+                pixel.blue = 0;
+                pixel.green = 0;
+                pixel.red = 0;
+                row.push_back(pixel);
+            }
+            temporalPixels.push_back(row);
+        }
+        return temporalPixels;
+    }
+
+
+
     // 1. Copy the original image (bmpPixelsData) to a new image called temporalPixelsForConvolution
 
-    // 2. Add 0s surrounding the image. As the kernel is a 5x5 kernel, add 2 rows and 2 columns of 0s
+    int originalHeight = bmpPixelsData.size();
+    int originalWidth = bmpPixelsData[0].size();
+    BmpPixels temporalPixelsForConvolution = bmpPixelsData;
+
+
+
+    // 2. Resize temporalPixelsForConvolution to be 4 pixels bigger in each side and fill it with 0s.
+    
+    temporalPixelsForConvolution.resize(originalHeight + 4);
+    for (int i = 0; i < originalHeight + 4; i++) {
+        temporalPixelsForConvolution[i].resize(originalWidth + 4);
+    }
+    for (int i = 0; i < originalHeight + 4; i++) {
+        for (int j = 0; j < originalWidth + 4; j++) {
+            temporalPixelsForConvolution[i][j].red = 0;
+            temporalPixelsForConvolution[i][j].green = 0;
+            temporalPixelsForConvolution[i][j].blue = 0;
+        }
+    }
+
+
 
     // 3. Iterate over the temporalPixelsForConvolution starting: i = 2, j = 2
     //    and ending: i = temporalPixelsForConvolution.size() - 2, j = temporalPixelsForConvolution[0].size() - 2
@@ -33,6 +74,7 @@ void ImageAOS::ApplyGaussianBlur() {
         for(long unsigned int j = 2; j < temporalPixelsForConvolution[0].size() - 2;j++){
             // 4. For each pixel, apply the convolution operation and store the result in the original image (bmpPixelsData)
             // store it in bmpPixelsData[i-2][j-2]
+            bmpPixelsData[i-2][j-2].red = 
         }
     }
 

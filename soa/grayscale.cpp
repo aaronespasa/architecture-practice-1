9e7d2b259078969401a5d258b99267d105efdd6e
aaronespasa <aaespasa@gmail.com>
2022-10-25 13:15:15 +0200

    Solving issue on the grayscale SOA code

--- soa/grayscale.cpp
+++ soa/grayscale.cpp
@@ -8,70 +8,56 @@ using namespace std;
 
 struct PixelNormalized {
 
-    vector<vector<uint8_t>> blue;
-    vector<vector<uint8_t>> green;
-    vector<vector<uint8_t>> red;
-    vector<vector<uint8_t>> gray;
+    vector<vector<float>> blue;
+    vector<vector<float>> green;
+    vector<vector<float>> red;
+    vector<vector<float>> gray;
 };
 
 //This function is used to normalize the pixel values
 //from 0 to 255 to 0.0 to 1.0
-uint8_t Normalize( uint8_t& mypixel) {
-    uint8_t pixelNormalized;
-
-    pixelNormalized = mypixel/ 255.0;
-   
-
-    return pixelNormalized;
+float Normalize( uint8_t& mypixel) {
+    return mypixel / 255.0;
 }
 
 //This function is used to correct the linear intensity of the pixel
-uint8_t Intensidad_lineal ( uint8_t& mypixel ) {
+float Intensidad_lineal ( float& mypixel ) {
     if (mypixel  <= 0.04045) {
-        mypixel = mypixel / 12.92;
+        return mypixel / 12.92;
     } else {
-        mypixel = std::pow(((mypixel + 0.055) / 1.055), 2.4);
-    }
-
-    return mypixel;        
+        return std::pow(((mypixel + 0.055) / 1.055), 2.4);
+    }     
 }
 
 //This function converges the 3 values of the pixel to a single value(gray)
 
-uint8_t Transformacion_lineal( uint8_t& mypixel_red, uint8_t& mypixel_green, uint8_t& mypixel_blue) {
-    uint8_t pixel_gray;
-     pixel_gray= 0.2126 * mypixel_blue + 0.7152 * mypixel_green + 0.0722 * mypixel_red;
-    
-    return pixel_gray;
-
+float Transformacion_lineal( float& mypixel_red, float& mypixel_green, float& mypixel_blue) {
+    return 0.2126 * mypixel_blue + 0.7152 * mypixel_green + 0.0722 * mypixel_red;
 }
 
 //This function is used to apply the gamma correction of the pixel
 
-uint8_t correccion_gamma ( uint8_t& mypixel ) {
+float correccion_gamma ( float& mypixel ) {
     if (mypixel  <= 0.0031308) {
-        mypixel = mypixel * 12.92;
+        return mypixel * 12.92;
     }
     else{
-        mypixel = 1.055 * std::pow(mypixel, 1/2.4) - 0.055;
-    }
-
-    return mypixel;        
+        return 1.055 * std::pow(mypixel, 1/2.4) - 0.055;
+    }     
 }
 
 //This function is used to denormalize the pixel value
-uint8_t Denormalize( uint8_t& mypixel, uint8_t& mypixel_gray) {
-    
+uint8_t Denormalize(float& mypixel_gray) {
     mypixel_gray = mypixel_gray * 255;
-    mypixel = floor(mypixel_gray + 0.5);
-    
-
-    return mypixel;
+    return floor(mypixel_gray + 0.5);
 }
-
 
 void ImageSOA::ToGrayScale() {
     PixelNormalized bmpPixelsDataNormalized;
+    bmpPixelsDataNormalized.blue.resize(bmpPixelsData.blue.size(), vector<float>(bmpPixelsData.blue[0].size()));
+    bmpPixelsDataNormalized.green.resize(bmpPixelsData.green.size(), vector<float>(bmpPixelsData.green[0].size()));
+    bmpPixelsDataNormalized.red.resize(bmpPixelsData.red.size(), vector<float>(bmpPixelsData.red[0].size()));
+    bmpPixelsDataNormalized.gray.resize(bmpPixelsData.blue.size(), vector<float>(bmpPixelsData.blue[0].size()));
 
     //Read all pixels and convert them to grayscale
     for (long unsigned int i = 0; i < bmpPixelsData.blue.size(); i++) {
@@ -87,11 +73,10 @@ void ImageSOA::ToGrayScale() {
             bmpPixelsDataNormalized.gray[i][j]= Transformacion_lineal(bmpPixelsDataNormalized.red[i][j],bmpPixelsDataNormalized.green[i][j],bmpPixelsDataNormalized.blue[i][j]);
             bmpPixelsDataNormalized.gray[i][j] = correccion_gamma(bmpPixelsDataNormalized.gray[i][j]);
             
-
-            bmpPixelsData.blue[i][j] = Denormalize(bmpPixelsDataNormalized.blue[i][j],bmpPixelsDataNormalized.gray[i][j]);
-            bmpPixelsData.green[i][j] = Denormalize(bmpPixelsDataNormalized.green[i][j],bmpPixelsDataNormalized.gray[i][j]);
-            bmpPixelsData.red[i][j] = Denormalize(bmpPixelsDataNormalized.red[i][j],bmpPixelsDataNormalized.gray[i][j]);
-
+            uint8_t pixel_gray = Denormalize(bmpPixelsDataNormalized.gray[i][j]);
+            bmpPixelsData.blue[i][j] = pixel_gray;
+            bmpPixelsData.green[i][j] = pixel_gray;
+            bmpPixelsData.red[i][j] = pixel_gray;
         }
     }
 }
--- soa/imgsoa.cpp
+++ soa/imgsoa.cpp
@@ -12,7 +12,15 @@ int main() {
     // imgsoa.CopyBitmapFile("../images/balloon.bmp", "../images/balloon_copy.bmp");
     imgsoa.ToGrayScale();
 
-    // imgsoa.WriteBitmapFile("../images/balloon_blur.bmp");
+    // print the first 5 pixels from imgsoa bmpPixelsData
+    BmpPixels bmpPixelsData = imgsoa.GetBitmapPixelsData();
+    for (int i = 0; i < 5; i++) {
+        std::cout << "blue: " << std::to_string(bmpPixelsData.blue[0][i]) << std::endl;
+        std::cout << "green: " << std::to_string(bmpPixelsData.green[0][i]) << std::endl;
+        std::cout << "red: " << std::to_string(bmpPixelsData.red[0][i]) << std::endl;
+    }
+
+    imgsoa.WriteBitmapFile("../images/balloon_mono.bmp");
 
     return 0;
 }

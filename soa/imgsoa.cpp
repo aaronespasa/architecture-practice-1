95b5678d8318b7dcf577424abe00750709a6dad8
aaronespasa <aaespasa@gmail.com>
2022-10-25 12:48:38 +0200

    modifying soa main file

--- soa/imgsoa.cpp
+++ soa/imgsoa.cpp
@@ -4,8 +4,15 @@
 int main() {
     ImageSOA imgsoa;
 
-    imgsoa.CopyBitmapFile("../images/balloon.bmp", "../images/balloon_copy.bmp");
-    // imgsoa.ToGrayScale();
+    imgsoa.ReadBitmapFile("../images/balloon.bmp");
+
+    // imgsoa.GenerateHistogram("../histograms/balloon.hst");
+
+    // imgsoa.ApplyGaussianBlur();
+    // imgsoa.CopyBitmapFile("../images/balloon.bmp", "../images/balloon_copy.bmp");
+    imgsoa.ToGrayScale();
+
+    // imgsoa.WriteBitmapFile("../images/balloon_blur.bmp");
 
     return 0;
 }

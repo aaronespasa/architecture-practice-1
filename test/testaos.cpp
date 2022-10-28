79c007d272bfaf1f5537fa43ed5d946dc06f8c73
aaronespasa <aaespasa@gmail.com>
2022-10-28 16:51:29 +0200

    Simplifying test implementation

--- test/testaos.cpp
+++ test/testaos.cpp
@@ -1,5 +1,9 @@
 #include <gtest/gtest.h>
 #include <iostream>
+#include <string>
+#include <stdlib.h>
+#include <vector>
+#include <filesystem>
 #include "../common/common.h"
 #include "../aos/aos.h"
 
@@ -25,221 +29,50 @@
 
 //  }
     
- TEST(checkGaussianBlur,size) {
-     /*Test if the size of the bmp is equal before and after
-     applying Gaussian Blur*/
-     ImageAOS imgaos;
-     imgaos.ReadBitmapFile("../images/balloon.bmp");
-     BmpPixels bmpPixelsDataOriginal = imgaos.GetBitmapPixelsData();
-     imgaos.ApplyGaussianBlur();
-     BmpPixels bmpPixelsDataModified = imgaos.GetBitmapPixelsData();
+//  TEST(checkGaussianBlur,size) {
+//      /*Test if the size of the bmp is equal before and after
+//      applying Gaussian Blur*/
+//      ImageAOS imgaos;
+//      imgaos.ReadBitmapFile("../images");
+    
+//      BmpPixels bmpPixelsDataOriginal = imgaos.GetBitmapPixelsData();
+//      imgaos.ApplyGaussianBlur();
+//      BmpPixels bmpPixelsDataModified = imgaos.GetBitmapPixelsData();
 
-     EXPECT_EQ(bmpPixelsDataOriginal.size(), bmpPixelsDataModified.size());
-     EXPECT_EQ(bmpPixelsDataOriginal[0].size(), bmpPixelsDataModified[0].size());
- }
+//      EXPECT_EQ(bmpPixelsDataOriginal.size(), bmpPixelsDataModified.size());
+//      EXPECT_EQ(bmpPixelsDataOriginal[0].size(), bmpPixelsDataModified[0].size());
+//  }
 
 
 
 
 // /* ----------------------------gray scale------------------------------------ */
 
-// TEST(check_gray_scale, balloon) {
-//    /* We are testing if the file porduced is the expected one by comparing 
-//       the bitmap of the image provided and the bitmap of the image produced  */
-//     ImageAOS imgaos;
-//     /* Aqui debe ir la lectura de la imagen de prueba  */
-//     imgaos.ReadBitmapFile("../images/images_test/gray_scale/balloon.bmp");
-//     BmpPixels bmpPixelsData_test = imgaos.GetBitmapPixelsData();
+TEST(check_gray_scale, all) {
+   /* We are testing if the file porduced is the expected one by comparing 
+      the bitmap of the image provided and the bitmap of the image produced  */
+    std::string original_images_path = "../images";
+    std::vector<std::string> original_images = getFiles(original_images_path);
 
-//     /* ----------------------------------------------- */
+    std::string transformed_images_path = "../images/images_test/gray_scale";
+    std::string expected_images_path = "../images/images_test/gray_scale";
+    std::string comparison_command;
 
-//     imgaos.ReadBitmapFile("../images/balloon.bmp");
-//     BmpPixels bmpPixelsData = imgaos.GetBitmapPixelsData();
-//     long unsigned int size_before = bmpPixelsData.size();
-//     imgaos.ToGrayScale();
-//     long unsigned int size_after = bmpPixelsData.size();
-//     /* EXPECT_EQ(size_before, size_after); */
-//         for (long unsigned int i = 0; i < bmpPixelsData.size(); i++) {
-//             for (long unsigned int j = 0; j < bmpPixelsData[0].size(); j++) {
-//                 EXPECT_EQ(bmpPixelsData[i][j].blue, bmpPixelsData_test[i][j].blue);
-//                 EXPECT_EQ(bmpPixelsData[i][j].green, bmpPixelsData_test[i][j].green);
-//                 EXPECT_EQ(bmpPixelsData[i][j].red, bmpPixelsData_test[i][j].red);
-//             }
-//         }
-// }
+    int output{-1};
+    for(std::string file : original_images) {
+        std::string filename = file + ".bmp";
 
-//  TEST(check_gray_scale, amphora) {
-//     /* We are testing if the file porduced is the expected one by comparing 
-//        the bitmap of the image provided and the bitmap of the image produced  */
-//      ImageAOS imgaos;
-//      /* Aqui debe ir la lectura de la imagen de prueba  */
-//      imgaos.ReadBitmapFile("../images/images_test/gray_scale/amphora.bmp");
-//      BmpPixels bmpPixelsData_test = imgaos.GetBitmapPixelsData();
+        ImageAOS imgaos;
+        imgaos.ReadBitmapFile(original_images_path + "/" + filename);
+        imgaos.ToGrayScale();
+        imgaos.WriteBitmapFile(transformed_images_path + "/" + filename);
 
-//      /* ----------------------------------------------- */
+        std::string transformed_image = transformed_images_path + "/" + filename;
+        std::string expected_image = expected_images_path + "/" + filename;
+        comparison_command = "cmp  " + transformed_image + " " + expected_image;
+        output = system(comparison_command.c_str());
+        EXPECT_EQ(output, 0);
+    }
+}
 
-//      imgaos.ReadBitmapFile("../images/amphora.bmp");
-//      BmpPixels bmpPixelsData = imgaos.GetBitmapPixelsData();
-//      long unsigned int size_before = bmpPixelsData.size();
-//      imgaos.ToGrayScale();
-//      long unsigned int size_after = bmpPixelsData.size();
-
-//      for (long unsigned int i = 0; i < bmpPixelsData.size(); i++) {
-//             for (long unsigned int j = 0; j < bmpPixelsData[0].size(); j++) {
-//                 EXPECT_EQ(bmpPixelsData[i][j].blue, bmpPixelsData_test[i][j].blue);
-//                 EXPECT_EQ(bmpPixelsData[i][j].green, bmpPixelsData_test[i][j].green);
-//                 EXPECT_EQ(bmpPixelsData[i][j].red, bmpPixelsData_test[i][j].red);
-//             }
-//         }
-//  }
-
-//  TEST(check_gray_scale, car) {
-//     /* We are testing if the file porduced is the expected one by comparing 
-//        the bitmap of the image provided and the bitmap of the image produced  */
-//      ImageAOS imgaos;
-//      /* Aqui debe ir la lectura de la imagen de prueba  */
-//      imgaos.ReadBitmapFile("../images/images_test/gray_scale/car.bmp");
-//      BmpPixels bmpPixelsData_test = imgaos.GetBitmapPixelsData();
-
-//      /* ----------------------------------------------- */
-
-//      imgaos.ReadBitmapFile("../images/car.bmp");
-//      BmpPixels bmpPixelsData = imgaos.GetBitmapPixelsData();
-//      long unsigned int size_before = bmpPixelsData.size();
-//      imgaos.ToGrayScale();
-//      long unsigned int size_after = bmpPixelsData.size();
-
-//      for (long unsigned int i = 0; i < bmpPixelsData.size(); i++) {
-//             for (long unsigned int j = 0; j < bmpPixelsData[0].size(); j++) {
-//                 EXPECT_EQ(bmpPixelsData[i][j].blue, bmpPixelsData_test[i][j].blue);
-//                 EXPECT_EQ(bmpPixelsData[i][j].green, bmpPixelsData_test[i][j].green);
-//                 EXPECT_EQ(bmpPixelsData[i][j].red, bmpPixelsData_test[i][j].red);
-//             }
-//         }
-//  }
-
-//  TEST(check_gray_scale, elephant) {
-//     /* We are testing if the file porduced is the expected one by comparing 
-//        the bitmap of the image provided and the bitmap of the image produced  */
-//      ImageAOS imgaos;
-//      /* Aqui debe ir la lectura de la imagen de prueba  */
-//      imgaos.ReadBitmapFile("../images/images_test/gray_scale/elephant.bmp");
-//      BmpPixels bmpPixelsData_test = imgaos.GetBitmapPixelsData();
-
-//      /* ----------------------------------------------- */
-
-//      imgaos.ReadBitmapFile("../images/elephant.bmp");
-//      BmpPixels bmpPixelsData = imgaos.GetBitmapPixelsData();
-//      long unsigned int size_before = bmpPixelsData.size();
-//      imgaos.ToGrayScale();
-//      long unsigned int size_after = bmpPixelsData.size();
-
-//      for (long unsigned int i = 0; i < bmpPixelsData.size(); i++) {
-//             for (long unsigned int j = 0; j < bmpPixelsData[0].size(); j++) {
-//                 EXPECT_EQ(bmpPixelsData[i][j].blue, bmpPixelsData_test[i][j].blue);
-//                 EXPECT_EQ(bmpPixelsData[i][j].green, bmpPixelsData_test[i][j].green);
-//                 EXPECT_EQ(bmpPixelsData[i][j].red, bmpPixelsData_test[i][j].red);
-//             }
-//         }
-//  }
-
-//  TEST(check_gray_scale, landscape) {
-//     /* We are testing if the file porduced is the expected one by comparing 
-//        the bitmap of the image provided and the bitmap of the image produced  */
-//      ImageAOS imgaos;
-//      /* Aqui debe ir la lectura de la imagen de prueba  */
-//      imgaos.ReadBitmapFile("../images/images_test/gray_scale/landscape.bmp");
-//      BmpPixels bmpPixelsData_test = imgaos.GetBitmapPixelsData();
-
-//      /* ----------------------------------------------- */
-
-//      imgaos.ReadBitmapFile("../images/landscape.bmp");
-//      BmpPixels bmpPixelsData = imgaos.GetBitmapPixelsData();
-//      long unsigned int size_before = bmpPixelsData.size();
-//      imgaos.ToGrayScale();
-//      long unsigned int size_after = bmpPixelsData.size();
-
-//      for (long unsigned int i = 0; i < bmpPixelsData.size(); i++) {
-//             for (long unsigned int j = 0; j < bmpPixelsData[0].size(); j++) {
-//                 EXPECT_EQ(bmpPixelsData[i][j].blue, bmpPixelsData_test[i][j].blue);
-//                 EXPECT_EQ(bmpPixelsData[i][j].green, bmpPixelsData_test[i][j].green);
-//                 EXPECT_EQ(bmpPixelsData[i][j].red, bmpPixelsData_test[i][j].red);
-//             }
-//         }
-//  }
-
-//  TEST(check_gray_scale, plane) {
-//     /* We are testing if the file porduced is the expected one by comparing 
-//        the bitmap of the image provided and the bitmap of the image produced  */
-//      ImageAOS imgaos;
-//      /* Aqui debe ir la lectura de la imagen de prueba  */
-//      imgaos.ReadBitmapFile("../images/images_test/gray_scale/plane.bmp");
-//      BmpPixels bmpPixelsData_test = imgaos.GetBitmapPixelsData();
-
-//      /* ----------------------------------------------- */
-
-//      imgaos.ReadBitmapFile("../images/planes.bmp");
-//      BmpPixels bmpPixelsData = imgaos.GetBitmapPixelsData();
-//      long unsigned int size_before = bmpPixelsData.size();
-//      imgaos.ToGrayScale();
-//      long unsigned int size_after = bmpPixelsData.size();
-
-//      for (long unsigned int i = 0; i < bmpPixelsData.size(); i++) {
-//             for (long unsigned int j = 0; j < bmpPixelsData[0].size(); j++) {
-//                 EXPECT_EQ(bmpPixelsData[i][j].blue, bmpPixelsData_test[i][j].blue);
-//                 EXPECT_EQ(bmpPixelsData[i][j].green, bmpPixelsData_test[i][j].green);
-//                 EXPECT_EQ(bmpPixelsData[i][j].red, bmpPixelsData_test[i][j].red);
-//             }
-//         }
-//  }
-
-//  TEST(check_gray_scale, pyramid) {
-//     /* We are testing if the file porduced is the expected one by comparing 
-//        the bitmap of the image provided and the bitmap of the image produced  */
-//      ImageAOS imgaos;
-//      /* Aqui debe ir la lectura de la imagen de prueba  */
-//      imgaos.ReadBitmapFile("../images/images_test/gray_scale/pyramid.bmp");
-//      BmpPixels bmpPixelsData_test = imgaos.GetBitmapPixelsData();
-
-//      /* ----------------------------------------------- */
-//      imgaos.ReadBitmapFile("../images/pyramid.bmp");
-//      BmpPixels bmpPixelsData = imgaos.GetBitmapPixelsData();
-//      long unsigned int size_before = bmpPixelsData.size();
-//      imgaos.ToGrayScale();
-//      long unsigned int size_after = bmpPixelsData.size();
-
-//      for (long unsigned int i = 0; i < bmpPixelsData.size(); i++) {
-//             for (long unsigned int j = 0; j < bmpPixelsData[0].size(); j++) {
-//                 EXPECT_EQ(bmpPixelsData[i][j].blue, bmpPixelsData_test[i][j].blue);
-//                 EXPECT_EQ(bmpPixelsData[i][j].green, bmpPixelsData_test[i][j].green);
-//                 EXPECT_EQ(bmpPixelsData[i][j].red, bmpPixelsData_test[i][j].red);
-//             }
-//         }
-//  }
-
-//  TEST(check_gray_scale, snake) {
-//     /* We are testing if the file porduced is the expected one by comparing 
-//        the bitmap of the image provided and the bitmap of the image produced  */
-//      ImageAOS imgaos;
-//      /* Aqui debe ir la lectura de la imagen de prueba  */
-//      imgaos.ReadBitmapFile("../images/images_test/gray_scale/snake.bmp");
-//      BmpPixels bmpPixelsData_test = imgaos.GetBitmapPixelsData();
-
-//      /* ----------------------------------------------- */
-
-//      imgaos.ReadBitmapFile("../images/snake.bmp");
-//      BmpPixels bmpPixelsData = imgaos.GetBitmapPixelsData();
-//      long unsigned int size_before = bmpPixelsData.size();
-//      imgaos.ToGrayScale();
-//      long unsigned int size_after = bmpPixelsData.size();
-
-//      for (long unsigned int i = 0; i < bmpPixelsData.size(); i++) {
-//             for (long unsigned int j = 0; j < bmpPixelsData[0].size(); j++) {
-//                 EXPECT_EQ(bmpPixelsData[i][j].blue, bmpPixelsData_test[i][j].blue);
-//                 EXPECT_EQ(bmpPixelsData[i][j].green, bmpPixelsData_test[i][j].green);
-//                 EXPECT_EQ(bmpPixelsData[i][j].red, bmpPixelsData_test[i][j].red);
-//             }
-//         }
-//  }
-
-/* ----------------------------gray scale------------------------------------ */ 
+/* ----------------------------------------------------------------------- */ 
